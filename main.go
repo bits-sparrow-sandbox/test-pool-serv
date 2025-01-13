34e427801fa4f55d3953ad821f5c9fc130d300d8
Cheng Dian Yang <361242902@qq.com>
2025-01-13 16:41:50 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,26 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message", "hello world",
+		})
+	})
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message", "service is health",
+		})
+	})
+
+	r.Run(":80")
+
+}
 
